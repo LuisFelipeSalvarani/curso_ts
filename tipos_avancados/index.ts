5ad2622ea11a6c83bead37bd2a29ead720fe617c
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-08-23 16:17:51 -0300

    Arrays

---
+++ tipos_avancados/index.ts
@@ -0,0 +1,12 @@
+// 1 - arrays
+let numbers: number[] = [1, 2, 3]
+
+numbers.push(5)
+
+console.log(numbers[2])
+
+// numbers = "teste"
+
+const nomes: string[] = ["Felipe", "Carlos"]
+
+// nomes.push(4)
