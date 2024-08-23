0b267d3a0ff022ec588fc0407ce35aa92827731c
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-08-23 14:11:30 -0300

    Tipo string

--- fundamentos/index.js
+++ fundamentos/index.js
@@ -7,3 +7,11 @@ var y = 15.584848;
 console.log(typeof y);
 console.log(y);
 console.log(y.toPrecision(3));
+// 2 - string
+var firstName = "Felipe";
+console.log(firstName.toUpperCase());
+var fullName;
+var lastName = "Salvarani";
+fullName = firstName + " " + lastName;
+console.log(fullName);
+console.log(typeof fullName);
--- fundamentos/index.ts
+++ fundamentos/index.ts
@@ -14,3 +14,18 @@ console.log(typeof y)
 console.log(y)
 
 console.log(y.toPrecision(3))
+
+// 2 - string
+const firstName: string = "Felipe"
+
+console.log(firstName.toUpperCase())
+
+let fullName: string
+
+const lastName: string = "Salvarani"
+
+fullName = firstName + " " + lastName
+
+console.log(fullName)
+
+console.log(typeof fullName)
