b0caac711f28aba1d9da08384521e93adc4e37b7
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-08-26 15:56:12 -0300

    instance of

--- narrowing/index.ts
+++ narrowing/index.ts
@@ -33,3 +33,36 @@ function operations(arr: number[], operation?: string | undefined) {
 operations([1, 2, 3])
 operations([1, 2, 3], "sum")
 operations([2, 4, 8], "multiply")
+
+// 3 - intance of
+
+class User {
+    name
+
+    constructor(name: string) {
+        this.name = name
+    }
+}
+
+class SuperUser extends User {
+    constructor(name: string) {
+        super(name)
+    }
+}
+
+const jhon = new User("Jhon")
+const paul = new SuperUser("Paul")
+
+console.log(jhon)
+console.log(paul)
+
+function userGreeting(user: object) {
+    if(user instanceof SuperUser) {
+        console.log(`Olá ${user.name}, deseja ver os dados do sistema?`)
+    } else if (user instanceof User) {
+        console.log(`Olá ${user.name}`)
+    }
+}
+
+userGreeting(jhon)
+userGreeting(paul)
