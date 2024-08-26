35cdf6152785284b77022d392c530cf344547954
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-08-26 15:00:06 -0300

    Symbol

--- tipos_avancados/index.ts
+++ tipos_avancados/index.ts
@@ -217,3 +217,11 @@ console.log(n)
 console.log(typeof n)
 
 console.log(n + 100n)
+
+// 18 - symbol
+
+let symbolA:symbol = Symbol("a")
+let symbolB = Symbol("a")
+
+console.log(symbolA == symbolB)
+console.log(symbolA === symbolB)
