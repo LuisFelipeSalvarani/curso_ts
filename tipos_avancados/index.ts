74d5411a6443bcb806b276a56ce6070d12627a77
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-08-26 14:46:51 -0300

    Interfaces

--- tipos_avancados/index.ts
+++ tipos_avancados/index.ts
@@ -137,3 +137,23 @@ function showId(id: ID) {
 showId(1)
 showId("200")
 showId(123)
+
+// 13 - interface
+
+interface Point {
+    x: number
+    y: number
+    z: number
+}
+
+function showCoords(obj: Point) {
+    console.log(`X: ${obj.x} Y: ${obj.y} Z: ${obj.z}`)
+}
+
+const coordObj: Point = {
+    x: 10,
+    y: 15,
+    z: 20
+}
+
+showCoords(coordObj)
