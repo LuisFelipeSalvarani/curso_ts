273b3b9a583f2735cb4c04555bfa044274d5b2c2
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-08-27 17:00:51 -0300

    Tipo de objeto com interface

--- object_types/index.js
+++ object_types/index.js
@@ -1,1 +1,14 @@
 "use strict";
+function showProductDetails(product) {
+    console.log(`O nome do produto é ${product.name} e seu preço é R$${product.price}`);
+    if (product.isAvailable) {
+        console.log("O produto está disponível");
+    }
+}
+const shirt = {
+    name: "Camisa",
+    price: 19.99,
+    isAvailable: true
+};
+showProductDetails(shirt);
+showProductDetails({ name: "Tênis", price: 129.99, isAvailable: false });
---
+++ object_types/index.ts
@@ -0,0 +1,22 @@
+// 1 - tipo de objeto para função com interface
+interface Product {
+    name: string
+    price: number
+    isAvailable: boolean
+}
+
+function showProductDetails(product:Product) {
+    console.log(`O nome do produto é ${product.name} e seu preço é R$${product.price}`)
+    if(product.isAvailable) {
+        console.log("O produto está disponível")
+    }
+}
+
+const shirt:Product = {
+    name: "Camisa",
+    price: 19.99,
+    isAvailable: true
+}
+
+showProductDetails(shirt)
+showProductDetails({name: "Tênis", price: 129.99, isAvailable: false})
