6a41d6a60004d7ea06baf7c7a8c7603bc70216f0
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-08-27 13:59:42 -0300

    Void

---
+++ funcoes/index.js
@@ -0,0 +1,7 @@
+"use strict";
+// 1 - Void
+function withoutReturn() {
+    console.log("Está função não tem retorno!");
+    // return 1
+}
+withoutReturn();
---
+++ funcoes/index.ts
@@ -0,0 +1,7 @@
+// 1 - Void
+function withoutReturn():void {
+    console.log("Está função não tem retorno!")
+    // return 1
+}
+
+withoutReturn()
