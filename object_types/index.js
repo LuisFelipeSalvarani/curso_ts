2bebf77278e476f47b007a4de93191ba60a443ef
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-08-27 17:32:45 -0300

    Readonly array

--- object_types/index.js
+++ object_types/index.js
@@ -54,3 +54,15 @@ const arnold = {
 };
 console.log(arnold);
 console.log(arnold.caliber);
+// 7 - readonly array
+let myArray = ["Maçã", "Laranja", "Banana"];
+// myArray[3] = "Mamão"
+console.log(myArray);
+myArray.forEach((item) => {
+    console.log("Fruta: " + item);
+});
+myArray = myArray.map((item) => {
+    return `Fruta: ${item}`;
+});
+console.log(myArray);
+// myArray[0] = "teste"
--- object_types/index.ts
+++ object_types/index.ts
@@ -123,3 +123,22 @@ const arnold: HumanWithGun = {
 console.log(arnold)
 
 console.log(arnold.caliber)
+
+// 7 - readonly array
+let myArray: ReadonlyArray<string> = ["Maçã", "Laranja", "Banana"]
+
+// myArray[3] = "Mamão"
+
+console.log(myArray)
+
+myArray.forEach((item) => {
+    console.log("Fruta: " + item)
+})
+
+myArray = myArray.map((item) => {
+    return `Fruta: ${item}`
+})
+
+console.log(myArray)
+
+// myArray[0] = "teste"
