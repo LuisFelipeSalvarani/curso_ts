54519afd08e1d97fba2bfd4d47b06b3ece974ff3
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-08-27 14:03:22 -0300

    Callback como argumento

--- funcoes/index.js
+++ funcoes/index.js
@@ -1,7 +1,18 @@
 "use strict";
-// 1 - Void
+// 1 - void
 function withoutReturn() {
     console.log("Está função não tem retorno!");
     // return 1
 }
 withoutReturn();
+// 2 - callback como argumento
+function greeting(name) {
+    return `Olá ${name}`;
+}
+function preGreeting(f, userName) {
+    console.log("Preparando a função");
+    const greet = f(userName);
+    console.log(greet);
+}
+preGreeting(greeting, "Felipe");
+preGreeting(greeting, "Luis");
--- funcoes/index.ts
+++ funcoes/index.ts
@@ -1,7 +1,23 @@
-// 1 - Void
+// 1 - void
 function withoutReturn():void {
     console.log("Está função não tem retorno!")
     // return 1
 }
 
 withoutReturn()
+
+// 2 - callback como argumento
+function greeting(name: string):string {
+    return `Olá ${name}`
+}
+
+function preGreeting(f: (name: string) => string, userName:string) {
+    console.log("Preparando a função")
+
+    const greet = f(userName)
+
+    console.log(greet)
+}
+
+preGreeting(greeting, "Felipe")
+preGreeting(greeting, "Luis")
