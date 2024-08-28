6c3adcc4a18cca04cdbb48047cb7619d290847dd
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2024-08-28 10:11:53 -0300

    Generic

--- criacao_de_tipos/index.js
+++ criacao_de_tipos/index.js
@@ -1,1 +1,9 @@
 "use strict";
+// 1 - generics
+function showData(arg) {
+    return `O dado é: ${arg}`;
+}
+console.log(showData(5));
+console.log(showData("testando generic"));
+console.log(showData(true));
+console.log(showData(["generic"]));
