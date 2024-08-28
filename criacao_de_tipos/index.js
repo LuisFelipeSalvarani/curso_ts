5016974cf89405ffd6b70eced6f76b60bb6183fe
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2024-08-28 10:16:36 -0300

    Constrint em generic

--- criacao_de_tipos/index.js
+++ criacao_de_tipos/index.js
@@ -7,3 +7,13 @@ console.log(showData(5));
 console.log(showData("testando generic"));
 console.log(showData(true));
 console.log(showData(["generic"]));
+// 2 - constraint em generic
+function showProductName(obj) {
+    return `O nome do produto é: ${obj.name}`;
+}
+const myObj = { name: "Porta", cor: "Branca" };
+const otherProduct = { name: "Carro", wheels: 4, engine: 1.0 };
+const thirdObj = { price: 19.99, category: "Roupa" };
+console.log(showProductName(myObj));
+console.log(showProductName(otherProduct));
+// console.log(showProductName(thirdObj))
