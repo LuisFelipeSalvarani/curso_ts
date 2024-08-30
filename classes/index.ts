b147197f2a9b314280eb17deef4994023aac3606
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2024-08-30 17:31:06 -0300

    Visibilidade: Private

--- classes/index.ts
+++ classes/index.ts
@@ -268,3 +268,36 @@ const fInstance = new F()
 fInstance.showX()
 
 fInstance.showProtecetedMethod()
+
+// 13 - visibilidade: private
+class PrivateClass {
+  private name = "Private"
+
+  showName() {
+    return this.name
+  }
+
+  private privateMethod() {
+    console.log("Método privado")
+  }
+
+  showPrivateMethod() {
+    this.privateMethod()
+  }
+}
+
+const pObj = new PrivateClass()
+
+// console.log(pObj.name)
+
+console.log(pObj.showName())
+
+// console.log(pObj.privateMethod())
+
+pObj.showPrivateMethod()
+
+// class TestingPrivate extends PrivateClass {
+//   myMethod() {
+//     this.privateMethod()
+//   }
+// }
