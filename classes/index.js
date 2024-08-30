b2ff35e2de3738e1028c9023eaf27c53f08afabd
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2024-08-30 16:43:34 -0300

    Setters

--- classes/index.js
+++ classes/index.js
@@ -86,3 +86,29 @@ class Person {
 const felipeSalvarani = new Person("Felipe", "Salvarani");
 console.log(felipeSalvarani.name);
 console.log(felipeSalvarani.fullName);
+// 8 - setters
+class Coords {
+    set fillX(x) {
+        if (x === 0) {
+            return;
+        }
+        this.x = x;
+        console.log("X inserido com sucesso");
+    }
+    set fillY(y) {
+        if (y === 0) {
+            return;
+        }
+        this.y = y;
+        console.log("Y inserido com sucesso");
+    }
+    get getCoords() {
+        return `X: ${this.x} e Y: ${this.y}`;
+    }
+}
+const myCoords = new Coords();
+myCoords.fillX = 15;
+myCoords.fillY = 0;
+myCoords.fillY = 10;
+console.log(myCoords);
+console.log(myCoords.getCoords);
