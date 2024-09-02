fbf3948fffadb03bde50da58dbc5efda8f40db7e
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2024-09-01 22:07:54 -0300

    Variáveis

--- react_ts/src/App.tsx
+++ react_ts/src/App.tsx
@@ -1,9 +1,17 @@
 import './App.css'
 
 export function App() {
+  // 1 - variáveis
+  const name: string = "Felipe"
+  const age: number = 24
+  const isWorking: boolean = true
+
   return (
     <>
       <h1>TypeScript com React</h1>
+      <h2>Nome: {name}</h2>
+      <p>Idade: {age}</p>
+      {isWorking && (<p>Está trabalhando</p>)}
     </>
   )
 }
