b2526981ee7d11594fa9c7a7dc73c5554ad587a5
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2024-09-01 22:10:34 -0300

    Funções

--- react_ts/src/App.tsx
+++ react_ts/src/App.tsx
@@ -6,12 +6,17 @@ export function App() {
   const age: number = 24
   const isWorking: boolean = true
 
+  // 2 - funções
+  const userGreeting = (name: string): string => {
+    return `Olá, ${name}!`
+  }
   return (
     <>
       <h1>TypeScript com React</h1>
       <h2>Nome: {name}</h2>
       <p>Idade: {age}</p>
       {isWorking && (<p>Está trabalhando</p>)}
+      <h3>{userGreeting(name)}</h3>
     </>
   )
 }
